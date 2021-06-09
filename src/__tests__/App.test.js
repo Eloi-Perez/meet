6db41e0e31277cb688b3cb854246e5cd5d1d98ae
Eloi-Perez <eloi104@hotmail.com>
2021-06-09 11:25:32 +0100

    spacing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,16 +6,16 @@ import CitySearch from '../CitySearch';
 
 
 describe('<App /> component', () => {
-  let AppWrapper;
-  beforeAll(() => {
-    AppWrapper = shallow(<App />);
-  });
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />);
+    });
 
-  test('render list of events', () => {
-    expect(AppWrapper.find(EventList)).toHaveLength(1);
-  });
+    test('render list of events', () => {
+        expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
 
-  test('render CitySearch', () => {
-    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-  });
+    test('render CitySearch', () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+    });
 });
