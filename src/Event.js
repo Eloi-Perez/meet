be8b97135e0829539ea21e4007e034e4276329be
Eloi-Perez <eloi104@hotmail.com>
2021-06-16 14:39:11 +0100

    render event + click to expand

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDetails: false,
+        };
+    }
     render() {
-        return <div></div>;
+        const { event } = this.props;
+        return (
+            <div className="event-container">
+                <h3>{event.summary}</h3>
+                <button onClick={() => this.setState({showDetails: !this.state.showDetails})}>Show Details</button>
+                {this.state.showDetails && (
+                    <div className="event-description">{event.description}</div>
+                )}
+            </div>
+        )
     }
 }
 export default Event;
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -7,11 +7,29 @@ import { mockData } from '../mock-data';
 describe('<Event /> component', () => {
     let EventWrapper;
     beforeAll(() => {
-        EventWrapper = shallow(<Event />);
+        EventWrapper = shallow(<Event event={mockData[0]} />);
     });
-
-    test('render event data/content', () => {
-        expect(EventWrapper.find(___)).toHaveLength(1);
+    test('render event', () => {
+        expect(EventWrapper.find(".event-container")).toHaveLength(1);
     });
-
+    test('render event summary', () => {
+        expect(EventWrapper.find("h3")).toHaveLength(1);
+        expect(EventWrapper.find("h3").text()).toEqual(mockData[0].summary);
+    });
+    test('showDetails false on init', () => {
+        expect(EventWrapper.state("showDetails")).toEqual(false);
+        expect(EventWrapper.find(".event-description")).toHaveLength(0);
+    });
+    test('on click show description', () => {
+        EventWrapper.find(".event-container button").at(0).simulate("click");
+        expect(EventWrapper.state("showDetails")).toEqual(true);
+        expect(EventWrapper.find(".event-description")).toHaveLength(1);
+        expect(EventWrapper.find(".event-description").text()).toEqual(mockData[0].description);
+    });
+        test('if open on click hide description', () => {
+        EventWrapper.setState({ showDetails: true});
+        EventWrapper.find(".event-container button").at(0).simulate("click");
+        expect(EventWrapper.state("showDetails")).toEqual(false);
+        expect(EventWrapper.find(".event-description")).toHaveLength(0);
+    });
 });
