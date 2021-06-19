1877db57a7710f0d12fba023906d9ddfb01f3031
Eloi-Perez <eloi104@hotmail.com>
2021-06-19 09:37:10 +0100

    components integration

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -21,6 +21,8 @@ class CitySearch extends Component {
         this.setState({
             query: suggestion
         });
+
+        this.props.updateEvents(suggestion);
     }
 
     render() {
@@ -36,7 +38,7 @@ class CitySearch extends Component {
                     {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
                     ))}
-                    <li key='all'>
+                    <li key='all' onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
                 </ul>
