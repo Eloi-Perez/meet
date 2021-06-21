39b3ac3675a274cbf732670d27a0947209e5f4a9
Eloi-Perez <eloi104@hotmail.com>
2021-06-22 00:02:12 +0100

    format

--- src/Event.js
+++ src/Event.js
@@ -15,11 +15,11 @@ class Event extends Component {
                 <h3>{event.summary}</h3>
                 <p>{date.toString()}</p>
                 <p>{event.location}</p>
-                <button className="details-btn" onClick={() => this.setState({showDetails: !this.state.showDetails})}>Show Details</button>
+                <button className="details-btn" onClick={() => this.setState({ showDetails: !this.state.showDetails })}>Show Details</button>
                 {this.state.showDetails && (
                     <div className="event-details">
-                    <p className="event-description">{event.description}</p>
-                    <p><a href={event.htmlLink}>Event in Google Calendar</a></p>
+                        <p className="event-description">{event.description}</p>
+                        <p><a href={event.htmlLink}>Event in Google Calendar</a></p>
                     </div>
                 )}
             </div>
