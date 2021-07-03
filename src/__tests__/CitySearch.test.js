126aff5dfb209c2e3d13a23ea28e716dc5f5ded0
Eloi-Perez <eloi104@hotmail.com>
2021-07-03 20:24:24 +0100

    fix path

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CitySearch from '../CitySearch';
-import { mockData } from '../mock-data';
+import CitySearch from '../components/CitySearch';
+import { mockData } from '../components/mock-data';
 import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
