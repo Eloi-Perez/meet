87855379cd4db0c38d4a11449bcfe6bbd7a8383b
Eloi-Perez <eloi104@hotmail.com>
2021-07-04 00:51:32 +0100

    add extra genre and color

--- src/components/GenreVisualization.js
+++ src/components/GenreVisualization.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
-const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#ff0048', '#a200ff'];
 
 const getData = (eventsSliced) => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS', 'Angular'];
     const data = genres.map((genre) => {
         const value = eventsSliced.filter(({ summary }) => summary.split(/[ ,.-]+/).includes(genre)).length;
-        if (value !== 0) {
-            return { name: genre, value };
-        } else {
+        if (value === 0) {
             return { name: 'Others', value };
+        } else {
+            return { name: genre, value };
         }
     });
     return data;
